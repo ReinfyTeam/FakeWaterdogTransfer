d11d5f3e9725de7f933bd64cddb67f46d5bdc8c7
Ace Sobremont <xqwtxon@hotmail.com>
2025-12-19 19:30:22 +0800

    Oops

--- tools/build.php
+++ tools/build.php
@@ -31,7 +31,7 @@ $to = getcwd() . DIRECTORY_SEPARATOR . "build" . DIRECTORY_SEPARATOR;
 @mkdir($to, 0777, true);
 
 copyDirectory($from . "src", $to . "src");
-copyDirectory($from . "resources", $to . "resources");
+//copyDirectory($from . "resources", $to . "resources");
 
 $pluginYml = yaml_parse_file($from . "plugin.yml");
 
